8736e1e0b06bd28169730a321e478215f8c77375
Kike <kike@areavisual.net>
2017-01-26 17:14:15 +0100

    Pruebas utf-8
    Hay problemas para resolver la lectura del excel y convertir a utf-8.

--- index.php
+++ index.php
@@ -3,50 +3,23 @@ require_once 'Excel/reader.php';
 
 $data = new Spreadsheet_Excel_Reader();
 $data->setOutputEncoding('CP1251'); // Character Encodings - Legacy Encodings - CP1251
+
 //$data->read('DatosClientes.xls');
-$data->read('Datos24012017053710Clientes.xls');
+//$data->read('Datos24012017053710Clientes.xls');
+$data->read('ClientesUT8.xls');
+
+
 ?>
 <html>
 <head>
+    <meta charset="utf-8">
     <title>Leer XLS</title>
     
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-    <meta charset="UTF-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-     <style type="text/css">
-         
-         #contenido { width: 100%; margin-left: 7%; }
-        /* 
-         .table-container
-            {
-                width: 100%;
-                overflow-y: auto;
-                _overflow: auto;
-                margin: 0 0 1em;
-            }
-
-         table, td {
-                align-content: center;
-                width: 60%;
-                background-color:#f0f0f0; 
-                font-family:verdana; 
-                font-size:90%;
-                border: 1px solid black;
-                padding: 10px;
-                border-spacing: 10px;
-                border-collapse: separate;
-         }
-         
-         h1 {
-                font-family: Arial, Helvetica, sans-serif;
-                font-style: normal;
-                color:purple;
-                text-align:center;
-                font:20px;
-         }
-         */
-         
+    <style type="text/css">     
     </style>
 
 
@@ -56,31 +29,54 @@ $data->read('Datos24012017053710Clientes.xls');
         <div class="table-responsive">
         <table class="table table-bordered">
 <?php      
-// Mostramos los datos en forma de tabla por medio de un bucle for
+
+
+
+    # Conexión base de datos MySql
+    $link = mysqli_connect("localhost","root","","");
+    if (mysqli_connect_errno()) {
+                die("Error al conectar: ".mysqli_connect_error());
+        }
+        
+//$tildes = $link->query("SET NAMES 'utf8'"); //Para que se muestren las tildes correctamente
 /*
-echo("<table>");
-for ($i = 1; $i <= $data->sheets[0]['numRows']; $i++) {
-	echo("<tr>");
-	for ($j = 1; $j <= $data->sheets[0]['numCols']; $j++) {
-		echo("<td>" . $data->sheets[0]['cells'][$i][$j] ."</td>");
-	}
-	echo("</tr>");	
-}
-echo("</table>");
+$result = mysqli_query($link, "SELECT * FROM Clientes");
+
+while (($fila = mysqli_fetch_array($result))!=NULL){
+
+               printf("<tr> 
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+               <td>%s</td>
+                    </tr>",          
+                $fila['codcli'], $fila['razon'], $fila['direccion'], $fila['codigopostal'], $fila['poblacion'], $fila['provincia'], 
+                $fila['telefono'], $fila['rotulo'], $fila['nif'], 
+                $fila['DtoFamilia01'], $fila['DtoFamilia02'], $fila['DtoFamilia03'], $fila['DtoFamilia04'], $fila['DtoFamilia05'], $fila['DtoFamilia06'], $fila['DtoFamilia07'], $fila['DtoFamilia08'], $fila['DtoFamilia09'], $fila['DtoFamilia10']);
+
+                }
+
+            # Libero la memoria asociada a result y cierro base de datos
+            mysqli_free_result($result);
 */
+            
 
-// Conectamos con la base de datos 
-/*
-$dbhost="localhost";  	// host del MySQL (generalmente localhost)
-$dbusuario="root"; 		// aqui debes ingresar el nombre de usuario para acceder a la base
-$dbpassword=""; 	// password de acceso para el usuario de la linea anterior
-$db="";     // Seleccionamos la base con la cual trabajar
-
-$conexion = mysql_connect($dbhost, $dbusuario, $dbpassword);
-mysql_select_db($db, $conexion);
-*/
-
-// Otra manera de leer el documento EXCEL y cada línea la introducimos en la base de datos creada
+# Leer el documento EXCEL y cada línea la introducimos en la base de datos creada
 
 for ($fila = 1; $fila <= $data->sheets[0]['numRows']; $fila++) {
 	echo("<tr>");
@@ -88,26 +84,15 @@ for ($fila = 1; $fila <= $data->sheets[0]['numRows']; $fila++) {
 	$vrazon = $data->sheets[0]['cells'][$fila][2]; 
 	$vdireccion = $data->sheets[0]['cells'][$fila][3];
 	$vcodigopostal = $data->sheets[0]['cells'][$fila][4];
+    
 	$vpoblacion = $data->sheets[0]['cells'][$fila][5];
+    iconv("CP1251", "utf-8", $vpoblacion);
+    
 	$vprovincia = $data->sheets[0]['cells'][$fila][6];
 	$vtelefono = $data->sheets[0]['cells'][$fila][7];
 	$vrotulo = $data->sheets[0]['cells'][$fila][8];
 	$vnif = $data->sheets[0]['cells'][$fila][9];
-	/*
-	echo $fila, 
-		" - ", $vcodcliente, 
-		" - ", $vrazon,
-		" - ", $vdireccion,
-		" - ", $vcodigopostal, 
-		" - ", $vpoblacion, 
-		" - ", $vprovincia, 
-		" - ", $vtelefono,
-		" - ", $vrotulo,
-		" - ", $vnif;  
 
-
-	echo("</tr>");echo("<BR>");
-    */
     printf("<tr>
             <td>%s</td>
             <td>%s</td>
@@ -131,21 +116,24 @@ for ($fila = 1; $fila <= $data->sheets[0]['numRows']; $fila++) {
             $vtelefono,
             $vrotulo,
             $vnif);
+    
+        /*
+        # Enviamos toda la infomación a la base de datos
+        $sql="INSERT INTO Clientes (codcli, razon, direccion, codigopostal, poblacion, provincia, telefono, rotulo, nif, DtoFamilia01, DtoFamilia02, DtoFamilia03, DtoFamilia04, DtoFamilia05, DtoFamilia06, DtoFamilia07, DtoFamilia08, DtoFamilia09, DtoFamilia10)
+        VALUES ('$vcodcliente', '$vrazon', '$vdireccion', '$vcodigopostal', '$vpoblacion', '$vprovincia', '$vtelefono', '$vrotulo', '$vnif','0','0','0','0','0','0','0','0','0','0')";
 
-	// Aquí cada línea que obtenemos de la hoja excel la introducimos en la base de datos MySQL
-	/*
-	$result = mysql_query("INSERT INTO clientes (codcli, razon, direccion, codigopostal, poblacion, provincia, telefono, rotulo, nif, DtoFamilia01, DtoFamilia02, DtoFamilia03, DtoFamilia04, DtoFamilia05, DtoFamilia06, DtoFamilia07, DtoFamilia08, DtoFamilia09, DtoFamilia10) VALUES ('$vcodcliente', '$vrazon', '$vdireccion', '$vcodigopostal', '$vpoblacion', '$vprovincia', '$vtelefono', '$vrotulo', '$nif','0','0','0','0','0','0','0','0','0','0')", $conexion);
-	if (!$result) {
-    				die('Consulta no válida: '.mysql_error());
-	}
-	*/
+        # Escribimos el registro correspondiente al índice de $fila en la base de datos
+        $result = mysqli_query($link, $sql);
+        */
 
 }
 
-/*
-// Cerramos la base de datos
-mysql_close($conexion);
-*/
+
+
+    # Desconectamos Base de datos       
+    $close = mysqli_close($link) 
+     or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
+
 ?>
         </table>
         </div>
